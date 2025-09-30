9eab6863357e19a5b553c64d66f27f559bb1ee47
Copojaab <palermijacopo@gmail.com>
2025-09-30 17:16:04 +0200

    project done

--- script.js
+++ script.js
@@ -1,39 +1,61 @@
 const containerDiv = document.querySelector("#divContainer");
+const controlsDiv = document.querySelector('#controls');
+
+function createUI(){
+    const btnChangeDim = document.createElement("button");
+    btnChangeDim.classList.add('changeDimension');
+    btnChangeDim.textContent = 'Cambia la dimensione della griglia';
+    controlsDiv.prepend(btnChangeDim);
+    return {btnChangeDim};
+}
+const ui = createUI();
+
+function checkInput(input){
+    input = Number.parseInt(input, 10);
+    if(Number.isNaN(input)){
+        alert('Inserisci un numero');
+        return null;
+    }
+    if(input <=0 || input > 100){
+        alert('Inserisci un valore tra 0 e 100');
+        return null;
+    }
+    return input;
+}
 
 function createGrid(size){
-    // svuoto il container
     containerDiv.innerHTML = '';
     const cellDimension = 960 / size;
-    // creo i div della griglia e li appendo alla lista
     for(let i = 0; i < size * size; i++){
         const divCell = document.createElement('div');
         divCell.classList.add('cell');
         divCell.style.height = cellDimension + 'px';
         divCell.style.width = cellDimension + 'px';
         containerDiv.appendChild(divCell);
-    }
+    } 
 }
 
-let gridSize = prompt("Inserisci la dimensione della griglia(max 100): ");
-// validity check
-gridSize = Number.parseInt(gridSize, 10);
-
-if(Number.isNaN(gridSize)){
-    alert("Inserisci un valore numerico");
+function aggiungiEventiCell() {
+    const allCells = document.querySelectorAll('.cell');
+    allCells.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+        });  
+    });
 }
 
-if(gridSize <= 0 || gridSize > 100){
-    alert("Inserisci un valore compreso tra 0 e 100.");
+function btnChangeDimension(){
+    ui.btnChangeDim.addEventListener('click', () =>{
+        const userPrompt = prompt('Inserisci la dimensione della griglia: ');
+        const newDimension = checkInput(userPrompt);
+        if (newDimension) {
+            createGrid(newDimension);
+            aggiungiEventiCell();
+        }
+    })
 }
 
-console.log(gridSize);
-
+let gridSize = 16;
 createGrid(gridSize);
-
-const allCells = document.querySelectorAll('.cell');
-
-allCells.forEach(cell => {
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = 'black';
-    });    
-});
+aggiungiEventiCell();
+btnChangeDimension();
